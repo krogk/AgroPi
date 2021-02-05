256853f9bdbf95ede084cd52c618b5486ccd5cf5
Samuel Obosu <s.obosu@nsano.com>
2021-02-05 15:39:31 +0000

    Updated project name in web app

--- software/web/app/config.py
+++ software/web/app/config.py
@@ -14,7 +14,7 @@ MAIL_DEFAULT_SENDER = 'noreply@yourmail.com'
 
 # project settings
 PROJECT_PASSWORD_HASH_METHOD = 'pbkdf2:sha1'
-PROJECT_SITE_NAME = u'Flask Example'
+PROJECT_SITE_NAME = u'AgroPi'
 PROJECT_SITE_URL = u'http://127.0.0.1:5000'
 PROJECT_SIGNUP_TOKEN_MAX_AGE = 60 * 60 * 24 * 7  # in seconds
 PROJECT_RECOVER_PASSWORD_TOKEN_MAX_AGE = 60 * 60 * 24 * 7  # in seconds
