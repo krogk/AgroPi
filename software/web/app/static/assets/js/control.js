64da8da5464a1f2588e7d20dc83bd6f9f1f41796
Samuel Obosu <s.obosu@nsano.com>
2021-04-19 00:35:03 +0000

    Actuation updates

--- software/web/app/static/assets/js/control.js
+++ software/web/app/static/assets/js/control.js
@@ -72,68 +72,205 @@ $(document).ready(function(){
 
 });
 
+function update_temperature_value(){
+    var temperature_val = $('#temperature-slider').val().split(',');
 
+    temp_max = {'operation': 3, 'value': temperature_val[1]}
+    temp_min = {'operation': 4, 'value': temperature_val[0]}
 
-function update_control_values(){
-	var temperature_val = $('#temperature-slider').val().split(',');
-	var humidity_val = $('#humidity-slider').val().split(',');
-	var light_intensity_val = $('#light-slider').val().split(',');
-	var ethanol_val = $('#ethanol-slider').val().split(',');
+    post_data(temp_max);
+    post_data(temp_min, true);
+}
+
+function update_humidity_value(){
+    var humidity_val = $('#humidity-slider').val().split(',');
+
+    hum_max = {'operation': 13, 'value': humidity_val[1]}
+    hum_min = {'operation': 14, 'value': humidity_val[0]}
+
+    post_data(hum_max);
+    post_data(hum_min, true);
+}
+
+function update_light_value(){
+    var light_intensity_val = $('#light-slider').val().split(',');
+
+    light_max = {'operation': 1, 'value': light_intensity_val[1]}
+    light_min = {'operation': 2, 'value': light_intensity_val[0]}
+
+    post_data(hum_max);
+    post_data(hum_min, true);
+}
+
+function update_ethanol_value(){
+    var ethanol_val = $('#ethanol-slider').val().split(',');
+
+    eth_max = {'operation': 9, 'value': ethanol_val[1]}
+    eth_min = {'operation': 10, 'value': ethanol_val[0]}
+
+    post_data(eth_max);
+    post_data(eth_min, true);
+}
+
+function update_tvoc_value(){
     var tvoc_val = $('#tvoc-slider').val().split(',');
+
+    tvoc_max = {'operation': 5, 'value': tvoc_val[1]}
+    tvoc_min = {'operation': 6, 'value': tvoc_val[0]}
+
+    post_data(tvoc_max);
+    post_data(tvoc_min, true);
+}
+
+function update_eco2_value(){
     var eco2_val = $('#eco2-slider').val().split(',');
+
+    eco2_max = {'operation': 7, 'value': eco2_val[1]}
+    eco2_min = {'operation': 8, 'value': eco2_val[0]}
+
+    post_data(eco2_max);
+    post_data(eco2_min, true);
+}
+
+function update_h2_value(){
     var h2_val = $('#h2-slider').val().split(',');
-    var heating_toggle = $('#heating-toggle').prop('checked');
-    var airflow_toggle = $('#airflow-toggle').prop('checked');
-    var light_toggle = $('#lights-toggle').prop('checked');
 
-    if (heating_toggle == true){
-        heating_toggle = 1;
-    }
-    else{
-        heating_toggle = 0;
-    }
+    h2_max = {'operation': 11, 'value': h2_val[1]}
+    h2_min = {'operation': 12, 'value': h2_val[0]}
 
-    if (airflow_toggle == true){
-        airflow_toggle = 1;
-    }
-    else{
-        airflow_toggle = 0;
-    }
+    post_data(h2_max);
+    post_data(h2_min, true);
+}
 
-    if (light_toggle == true){
-        light_toggle = 1;
-    }
-    else{
-        light_toggle = 0;
-    }
+function update_heating(){
+    var heating = $('#heating-toggle').prop('checked');
+    var heating_heu = $('#heating-heu-toggle').prop('checked');
+
+    heating = {'operation': 128, 'value': heating}
+    heating_heu = {'operation': 132, 'value': heating_heu}
+
+    post_data(heating);
+    post_data(heating_heu, true);
+}
+
+function update_airflow(){
+    var airflow = $('#airflow-toggle').prop('checked');
+    var airflow_heu = $('#airflow-heu-toggle').prop('checked');
+
+    airflow = {'operation': 129, 'value': airflow}
+    airflow_heu = {'operation': 133, 'value': airflow_heu}
+
+    post_data(airflow);
+    post_data(airflow_heu, true);
+}
+
+function update_lights(){
+    var lights = $('#lights-toggle').prop('checked');
+    var lights_heu = $('#lights-heu-toggle').prop('checked');
+
+    lights = {'operation': 130, 'value': lights}
+    lights_heu = {'operation': 134, 'value': lights_heu}
+
+    post_data(lights);
+    post_data(lights_heu, true);
+}
+
+function update_water(){
+    var water = $('#water-toggle').prop('checked');
+    var water_heu = $('#water-heu-toggle').prop('checked');
+
+    water = {'operation': 131, 'value': water}
+    water_heu = {'operation': 135, 'value': water_heu}
+
+    post_data(water);
+    post_data(water_heu, true);
+}
 
 
-	form_data = {
-        'min_temp': temperature_val[0],
-        'max_temp': temperature_val[1],
-        'min_humidity': humidity_val[0],
-        'max_humidity': humidity_val[1],
-        'min_light': light_intensity_val[0],
-        'max_light': light_intensity_val[1],
-        'min_ethanol': ethanol_val[0],
-        'max_ethanol': ethanol_val[1],
-        'min_tvoc': tvoc_val[0],
-        'max_tvoc': tvoc_val[1],
-        'min_eco2': eco2_val[0],
-        'max_eco2': eco2_val[1],
-        'min_h2': h2_val[0],
-        'max_h2': h2_val[1],
-        'heating_toggle': heating_toggle,
-        'airflow_toggle': airflow_toggle,
-        'light_toggle': light_toggle
-    }
-    $('#update-control-button').attr('disabled', true);
-    // form_data = JSON.stringify(form_data);
-    console.log(form_data);
+
+// function update_control_values(){
+// 	var temperature_val = $('#temperature-slider').val().split(',');
+// 	var humidity_val = $('#humidity-slider').val().split(',');
+// 	var light_intensity_val = $('#light-slider').val().split(',');
+// 	var ethanol_val = $('#ethanol-slider').val().split(',');
+//     var tvoc_val = $('#tvoc-slider').val().split(',');
+//     var eco2_val = $('#eco2-slider').val().split(',');
+//     var h2_val = $('#h2-slider').val().split(',');
+//     var heating_toggle = $('#heating-toggle').prop('checked');
+//     var airflow_toggle = $('#airflow-toggle').prop('checked');
+//     var light_toggle = $('#lights-toggle').prop('checked');
+
+//     if (heating_toggle == true){
+//         heating_toggle = 1;
+//     }
+//     else{
+//         heating_toggle = 0;
+//     }
+
+//     if (airflow_toggle == true){
+//         airflow_toggle = 1;
+//     }
+//     else{
+//         airflow_toggle = 0;
+//     }
+
+//     if (light_toggle == true){
+//         light_toggle = 1;
+//     }
+//     else{
+//         light_toggle = 0;
+//     }
+
+//     data_list = [
+//         {'operation': 1, 'value': light_intensity_val[1]}, 
+//         {'operation': 2, 'value': light_intensity_val[0]}, 
+        
+//         {'operation': 5, 'value': tvoc_val[1]}, 
+//         {'operation': 6, 'value': tvoc_val[0]}, 
+//         {'operation': 7, 'value': eco2_val[1]}, 
+//         {'operation': 8, 'value': eco2_val[0]}, 
+//         {'operation': 9, 'value': ethanol_val[1]}, 
+//         {'operation': 10, 'value': ethanol_val[0]}, 
+//         {'operation': 11, 'value': h2_val[1]}, 
+//         {'operation': 12, 'value': h2_val[0]}, 
+//         {'operation': 13, 'value': humidity_val[1]}, 
+//         {'operation': 14, 'value': humidity_val[0]}, 
+//         {'operation': 128, 'value': heating_toggle}, 
+//         {'operation': 129, 'value': airflow_toggle}, 
+//         {'operation': 130, 'value': light_toggle}, 
+//     ]
+
+// 	// form_data = {
+//  //        'min_temp': temperature_val[0],
+//  //        'max_temp': temperature_val[1],
+//  //        'min_humidity': humidity_val[0],
+//  //        'max_humidity': humidity_val[1],
+//  //        'min_light': light_intensity_val[0],
+//  //        'max_light': light_intensity_val[1],
+//  //        'min_ethanol': ethanol_val[0],
+//  //        'max_ethanol': ethanol_val[1],
+//  //        'min_tvoc': tvoc_val[0],
+//  //        'max_tvoc': tvoc_val[1],
+//  //        'min_eco2': eco2_val[0],
+//  //        'max_eco2': eco2_val[1],
+//  //        'min_h2': h2_val[0],
+//  //        'max_h2': h2_val[1],
+//  //        'heating_toggle': heating_toggle,
+//  //        'airflow_toggle': airflow_toggle,
+//  //        'light_toggle': light_toggle
+//  //    }
+//     $('#update-control-button').attr('disabled', true);
+//     // form_data = JSON.stringify(form_data);
+//     console.log(form_data);
     
-    $.post("/sensor/:80", form_data)
+// }
+
+function post_data(data_, notify){
+    $.post("/sensor/:80", data_)
       .done(function( data ) {
-        notify_success("Values updated");
+        if(notify == true){
+            notify_success("Values updated");
+        }
     });
 
     // $.post("/sensor/:80", form_data, function( data ) {
@@ -151,3 +288,9 @@ function update_control_values(){
     //     $('#update-control-button').attr('disabled', false);
     // });
 }
+
+
+
+
+
+
